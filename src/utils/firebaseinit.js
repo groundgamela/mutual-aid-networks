1a27bdc7cd3ac794099ac2d78e833708fa63bd2a
meganrm <meganrm@gmail.com>
2020-03-16 16:58:44 -0700

    removed auth

--- src/utils/firebaseinit.js
+++ src/utils/firebaseinit.js
@@ -3,10 +3,9 @@
 import firebase from 'firebase';
 
 const PREFIX = process.env.NODE_ENV === "production" ? "REACT_APP_PROD" : "REACT_APP_TESTING";
-if (process.env.NODE_ENV !== "production") {
-  console.log(process.env[`${PREFIX}_PROJECT_ID`]);
-}
 
+
+console.log(process.env[`${PREFIX}_PROJECT_ID`]);
 const config = {
   apiKey: process.env[`${PREFIX}_FIREBASE_API_KEY`],
   authDomain: process.env[`${PREFIX}_FIREBASE_AUTH_DOMAIN`],
@@ -18,8 +17,6 @@ const config = {
 
 firebase.initializeApp(config);
 export const firebasedb = firebase.database();
-export const provider = new firebase.auth.GoogleAuthProvider();
-export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 export default firebase;
