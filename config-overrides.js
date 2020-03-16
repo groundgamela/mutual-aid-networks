0fa16519ff94a9a34254b464c1b9141bec983dcb
meganrm <meganrm@gmail.com>
2020-03-15 17:56:05 -0700

    added antd overrides

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+const {
+    override,
+    fixBabelImports,
+    addLessLoader
+} = require('customize-cra');
+
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+        
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {
+            '@primary-color': '#7f48f3'
+        },
+    }),
+);
