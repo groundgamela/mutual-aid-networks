942a2c2958c35d6e8c90dc88e87074d4500f6061
meganrm <meganrm@gmail.com>
2020-03-20 14:18:09 -0700

    adding iframe and new access token

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import 'iframe-resizer';
 
 import configureStore from './state/store';
 // Containers
