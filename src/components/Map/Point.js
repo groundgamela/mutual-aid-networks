b5e098d6d53686487fedf31411126f75be142101
meganrm <meganrm@gmail.com>
2020-03-19 17:37:57 -0700

    jitter the points

--- src/components/Map/Point.js
+++ src/components/Map/Point.js
@@ -1,8 +1,10 @@
+import { random } from 'lodash';
+
 class Point {
     constructor(network) {
         this.type = 'Feature';
         this.geometry = {
-            coordinates: [Number(network.lng), Number(network.lat)],
+            coordinates: [Number(network.lng + random(-.02, .02)), Number(network.lat + random(-.02, .02))],
             type: 'Point',
         };
         const scale = network.bbox[3] - network.bbox[1];
@@ -12,6 +14,7 @@ class Point {
             socials: network.social || null,
             facebookPage: network.facebookPage,
             state: network.state,
+            city: network.city,
             title: network.title,
             generalForm: network.generalForm,
             supportRequestForm: network.supportRequestForm,
