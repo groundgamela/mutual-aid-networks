0bee54e6773b006857b1e3656fe285ea047b2de1
meganrm <meganrm@gmail.com>
2020-03-25 11:38:14 -0700

    rest call working, but needs to update on first return

--- src/state/networks/logic.js
+++ src/state/networks/logic.js
@@ -1,13 +1,18 @@
 import {
   createLogic
 } from "redux-logic";
-
+import {
+  mapValues,
+  values,
+  map,
+} from 'lodash';
 import {
   REQUEST_NETWORKS,
   SET_NETWORKS,
   REQUEST_FAILED
 } from "./reducers";
 import { GENERAL, REQUEST_SUPPORT, OFFER_SUPPORT, INFORMATION } from "../constants";
+import { object } from "prop-types";
 
 const fetchNetworks = createLogic({
   type: REQUEST_NETWORKS,
@@ -17,28 +22,52 @@ const fetchNetworks = createLogic({
   },
   process(deps) {
     const {
-      firestore,
+      httpClient,
     } = deps;
-    return firestore.collection('mutual_aid_networks').get()
+    const allNetworks = [];
+
+    const requestPage = (token) => {
+      return httpClient.get(`https://firestore.googleapis.com/v1/projects/townhallproject-86312/databases/(default)/documents/mutual_aid_networks/?pageToken=${token}`)
+         .then((snapshot) => {
+               snapshot.body.documents.forEach((doc, index) => {
+                 const data = doc.fields;
+                 const unpackedData = mapValues(data, (object) => {
+                   let newValues = values(object)[0];
+                   if (newValues.values) {
+                     newValues = map(newValues.values, obj => {
+                       return values(obj)[0];
+                     });
+                   }
+                   return newValues;
+                 })
+                 let category;
+                 if (unpackedData.generalForm || (unpackedData.supportRequestForm && unpackedData.supportOfferForm)) {
+                   category = GENERAL;
+                 } else if (unpackedData.supportRequestForm) {
+                   category = REQUEST_SUPPORT;
+                 } else if (unpackedData.supportOfferForm) {
+                   category = OFFER_SUPPORT;
+                 } else {
+                   category = INFORMATION;
+                 }
+                 allNetworks.push({
+                   ...unpackedData,
+                   id: index,
+                   category: category,
+                 })
+               });
+               if (snapshot.body.nextPageToken) {
+                  return requestPage(snapshot.body.nextPageToken)
+               }
+               return allNetworks;
+              })
+            }
+    
+    return httpClient.get('https://firestore.googleapis.com/v1/projects/townhallproject-86312/databases/(default)/documents/mutual_aid_networks/')
       .then((snapshot) => {
-        const allNetworks = snapshot.docs.map((doc, index) => {
-          const data = doc.data();
-          let category;
-          if (data.generalForm || (data.supportRequestForm && data.supportOfferForm)) {
-            category = GENERAL;
-          } else if (data.supportRequestForm) {
-            category = REQUEST_SUPPORT;
-          } else if (data.supportOfferForm) {
-            category = OFFER_SUPPORT;
-          } else {
-            category = INFORMATION;
-          }
-          return {
-            ...data,
-            id: index,
-            category: category,
-          }
-        });
+        if (snapshot.body.nextPageToken) {
+          return requestPage(snapshot.body.nextPageToken);
+        }
         return allNetworks;
       })
   }
