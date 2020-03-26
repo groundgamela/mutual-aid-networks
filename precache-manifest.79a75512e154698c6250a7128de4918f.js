a786e4010a02febfa7c91f095022ea79e2e9b8ce
meganrm <meganrm@gmail.com>
2020-03-26 15:29:29 -0700

    Updates

---
+++ precache-manifest.79a75512e154698c6250a7128de4918f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d754faef8e8f3ab79d3bd508130ba80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09db4dfde893f7a97b94",
+    "url": "/static/css/2.1405cc33.chunk.css"
+  },
+  {
+    "revision": "fd05dcc9f90fb144a4da",
+    "url": "/static/css/main.1674bd21.chunk.css"
+  },
+  {
+    "revision": "09db4dfde893f7a97b94",
+    "url": "/static/js/2.1ec3a674.chunk.js"
+  },
+  {
+    "revision": "b930611d2aff024e7b57c6719d2ffab7",
+    "url": "/static/js/2.1ec3a674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd05dcc9f90fb144a4da",
+    "url": "/static/js/main.54835882.chunk.js"
+  },
+  {
+    "revision": "bea4c1743c08bfc6acd9",
+    "url": "/static/js/runtime-main.612ffcb9.js"
+  },
+  {
+    "revision": "0c89ab42d93925b400327b293a6781d1",
+    "url": "/static/media/logo-transparent.0c89ab42.png"
+  },
+  {
+    "revision": "8b47d93192978a00b878dc9d5a364dd5",
+    "url": "/static/media/usa.8b47d931.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78622c23b4aff7333646be6b8fc4cb3f.js"
+  "/precache-manifest.79a75512e154698c6250a7128de4918f.js"
 );
 
 self.addEventListener('message', (event) => {
