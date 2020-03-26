05ed9f3ddef928ce5d611e8a7d9bd32a58fe50e2
meganrm <meganrm@gmail.com>
2020-03-26 15:19:31 -0700

    changing access token to env var

--- src/components/Map/constants.js
+++ src/components/Map/constants.js
@@ -1,3 +1,3 @@
 export const LAYER_NAME = 'networks-dots'
-export const accessToken = 'pk.eyJ1IjoidG93bmhhbGxwcm9qZWN0IiwiYSI6ImNrN3d1c3hhdDA1MmozbW80cW5oOWxzcHoifQ.rq66iVpULeHVB6PutWiunA';
+export const accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
 export const mapboxStyle = process.env.REACT_APP_MAPBOX_STYLE_URL;
