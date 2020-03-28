e451e48b81e3799ace1323dea1e71fd2519d327b
meganrm <meganrm@gmail.com>
2020-03-28 00:31:14 -0700

    add default style url

--- src/components/Map/constants.js
+++ src/components/Map/constants.js
@@ -1,3 +1,3 @@
 export const LAYER_NAME = 'networks-dots';
 export const accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
-export const mapboxStyle = process.env.REACT_APP_MAPBOX_STYLE_URL;
+export const mapboxStyle = process.env.REACT_APP_MAPBOX_STYLE_URL || "mapbox://styles/townhallproject/ck87rw1kt1g821is46uvwm08k";
