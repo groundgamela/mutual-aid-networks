621c36db0943ce29dbd854d265ff04a5c30d60bb
meganrm <meganrm@users.noreply.github.com>
2020-04-22 04:59:42 +0000

    deploy: b797ee8e5eb7365f0c8e86dab879d42d2a1ebd78

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.821bca87b15cf475ed1d7e0e5f131e0c.js"
+  "/precache-manifest.0d11c2cbabe13aac4f38781c583c10e1.js"
 );
 
 self.addEventListener('message', (event) => {
