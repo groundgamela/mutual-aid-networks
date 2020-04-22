9853f709ff4ae6344b0130906e75eacd009e2798
Krista Loven <krista.loven.87@gmail.com>
2020-04-21 18:26:07 -0700

    Hotline number added to NetworkCard

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,8 @@
 export const makeConstant = (branchOfState, actionType) => `${branchOfState}-${actionType}`;
 
+export const standardizePhoneNumber = (hotlineNumber) => {
+  let cleaned = ('' + hotlineNumber).replace(/\D/g, '')
+  let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+  return match ? `(${match[1]}) ${match[2]}-${match[3]}` : ''
+}
+
