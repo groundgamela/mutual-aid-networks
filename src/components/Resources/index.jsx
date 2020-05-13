ec26e1a41004921d17515b0018518a50e55b7f11
Krista Loven <krista.loven.87@gmail.com>
2020-05-12 22:59:43 -0700

    Update Resources page, move to category segments

--- src/components/Resources/index.jsx
+++ src/components/Resources/index.jsx
@@ -6,6 +6,44 @@ import { resourceList } from './constants'
 import SubmitButton from '../SubmitButton'
 
 const Resources = (props) => {
+
+  const parseResourceSections = () => {
+    return resourceList.map(resourceSection => {
+      return (
+        <div key={resourceSection.section}>
+          <h3 className='list-section-header'>{resourceSection.section}</h3>
+          {displayResourceSectionContent(resourceSection.content)}
+        </div>
+      )
+    })
+  }
+
+  const displayResourceSectionContent = (resources) => {
+    return (
+      <List
+        itemLayout='horizontal'
+        dataSource={resources}
+        renderItem={resource => (
+          <List.Item
+            extra={resource.image &&
+            <img
+              className='resource-image'
+              alt={resource.name}
+              src={resource.image}
+            />
+          }
+          >
+            <List.Item.Meta
+              title={<a className='list-title' target='_blank' href={resource.link}>{resource.name}</a>}
+              description={resource.description}
+            />
+          </List.Item>
+        )}
+      >
+      </List>
+    )
+  }
+
   return (
     <>
       <div className='resources page-container'>
@@ -13,18 +51,7 @@ const Resources = (props) => {
         <p>
           Starting your own Mutual Aid effort? Take a look at some of the invaluable resources community organizers have created:
         </p>
-        <List
-          itemLayout='horizontal'
-          dataSource={resourceList}
-          renderItem={resource => (
-            <List.Item className='list'>
-              <List.Item.Meta
-                title={<a className='list-title' target='blank' href={resource.link}>{resource.name}</a>}
-              />
-            </List.Item>
-          )}
-        >
-        </List>
+        {parseResourceSections()}
         <p>
           Thank you to all the mutual aid organizers, trainers, and other leaders out there doing this work and sharing best practices.
         </p>
