969b0003df0f38fb3251087dc1ba1d46ffa22694
meganrm <meganrm@gmail.com>
2020-11-23 11:47:13 -0800

    updating banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+
 import './style.scss';
 
 export default function Banner(){
 
   return (
-  <div className="banner">
-    <h3>
-    Organizing a mutual aid effort in your community? Join fellow organizers around the country for our weekly Best Practice sharing calls.  
-      <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSc0JyNonD-tUIR3KglFWMiaPe2RqPG0NJ2FeQTST7EzyCW6eA/viewform"> Sign up here.</a>
-    </h3>
-    
-
-  </div>
+    <div className="banner">
+      <h3>
+        Mutual aid is keeping communities afloat. Find a network near you and
+        give what you can this holiday season:{" "}
+        <a target="blank" href="https://www.supportmutualaid.org/">
+          {" "}
+          Support Mutual Aid
+        </a>
+      </h3>
+    </div>
   );
 };
 
