2ee1997f3f702f21a0f1aef72a5c5e84366b71b8
meganrm <meganrm@gmail.com>
2023-11-12 21:18:57 -0800

    change map style

--- src/components/Map/constants.js
+++ src/components/Map/constants.js
@@ -1,4 +1,4 @@
 export const NETWORK_LAYER_NAME = 'networks-dots';
 export const FOOD_RESOURCE_LAYER_NAME = "food-resources";
 export const accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
-export const mapboxStyle = process.env.REACT_APP_MAPBOX_STYLE_URL || "mapbox://styles/townhallproject/ck87rw1kt1g821is46uvwm08k";
+export const mapboxStyle = process.env.REACT_APP_MAPBOX_STYLE_URL || "mapbox://styles/mutualaid/clmmsz0v601vw01rd74oa86r3";
